aedb871531440cd6411981dbbc82949c42b865fd
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-10 13:02:55 +0200

    Initial commit

---
+++ src/test/java/com/testmaker/api/TestMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testmaker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
