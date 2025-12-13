2f04f6a71d5bde677ffea140d0702b158f7845cb
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-13 16:43:51 +0200

    Define a constructor that takes the message and sets the fields accordingly

--- src/main/java/com/testmaker/api/dto/exception/ExceptionResponse.java
+++ src/main/java/com/testmaker/api/dto/exception/ExceptionResponse.java
@@ -2,9 +2,18 @@ package com.testmaker.api.dto.exception;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Data
 public class ExceptionResponse {
     private String message;
     private String date;
     private String time;
+
+    public ExceptionResponse(String message) {
+        this.message = message;
+        this.date = LocalDate.now().toString();
+        this.time = LocalTime.now().toString();
+    }
 }
