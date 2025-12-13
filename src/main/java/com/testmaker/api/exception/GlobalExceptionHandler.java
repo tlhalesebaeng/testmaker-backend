abfc582713cf7b596190ea553193abfbd4d3645f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-13 16:49:01 +0200

    Catch the method argument not valid exception

--- src/main/java/com/testmaker/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/testmaker/api/exception/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.testmaker.api.exception;
 
+import com.testmaker.api.dto.exception.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ObjectError error = e.getBindingResult().getAllErrors().get(0); // Get the first error that caused this exception
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(error.getDefaultMessage()));
+    }
 }
