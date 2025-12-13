b3bc332903ebaeb2b327dc2f34b94a180a0f85d2
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-13 16:06:51 +0200

    Add the all args contructor and the data annotations and define fields required to create an account

--- src/main/java/com/testmaker/api/dto/auth/CreateAccountRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/CreateAccountRequest.java
@@ -1,4 +1,12 @@
 package com.testmaker.api.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class CreateAccountRequest {
+    private String username;
+    private String email;
+    private String password;
 }
