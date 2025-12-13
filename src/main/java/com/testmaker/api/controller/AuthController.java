cec29efef21fe43a509ae9e3a1e642b9cf941951
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-13 16:04:50 +0200

    Make the class a controller and add a /signup route that returns no content and a 201 status code

--- src/main/java/com/testmaker/api/controller/AuthController.java
+++ src/main/java/com/testmaker/api/controller/AuthController.java
@@ -1,4 +1,18 @@
 package com.testmaker.api.controller;
 
+import com.testmaker.api.dto.auth.CreateAccountRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("${api.prefix}/auth")
 public class AuthController {
+    @PostMapping("/signup")
+    public ResponseEntity<Object> signup(@RequestBody CreateAccountRequest requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
 }
