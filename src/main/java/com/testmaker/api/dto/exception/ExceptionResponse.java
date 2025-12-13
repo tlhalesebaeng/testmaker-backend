6d0339a31746564f79d0f13184207bdc0e7c340a
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-13 16:41:08 +0200

    Add the @data annotation and define fields

--- src/main/java/com/testmaker/api/dto/exception/ExceptionResponse.java
+++ src/main/java/com/testmaker/api/dto/exception/ExceptionResponse.java
@@ -1,4 +1,10 @@
 package com.testmaker.api.dto.exception;
 
+import lombok.Data;
+
+@Data
 public class ExceptionResponse {
+    private String message;
+    private String date;
+    private String time;
 }
