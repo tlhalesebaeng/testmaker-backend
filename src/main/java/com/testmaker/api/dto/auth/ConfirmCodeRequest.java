ddfa5bb466c05833055461c6b8a1ebf5a2a7ece2
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 09:21:15 +0200

    Validate the request code

--- src/main/java/com/testmaker/api/dto/auth/ConfirmCodeRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/ConfirmCodeRequest.java
@@ -1,10 +1,16 @@
 package com.testmaker.api.dto.auth;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class ConfirmCodeRequest {
+    @NotNull(message = "Invalid code provided! Please provide a valid code")
+    @Min(value = 100000, message = "Invalid code provided! Please provide a 6 digit code")
+    @Max(value = 999999, message = "Invalid code provided! Please provide a 6 digit code")
     private Integer code;
 }
