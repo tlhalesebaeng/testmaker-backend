937011d72446814d2de1951e3cf88280a13c6faa
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 08:57:45 +0200

    Add data methods, the all args constructor and define fields

--- src/main/java/com/testmaker/api/dto/auth/LoginRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/LoginRequest.java
@@ -1,4 +1,11 @@
 package com.testmaker.api.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LoginRequest {
+    private String username;
+    private String password;
 }
