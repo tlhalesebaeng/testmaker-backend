0a5bdcf91ba7290dedd09499e28e554615cbb0e5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 09:10:29 +0200

    Add data methods, the all args constructor and define class properties

--- src/main/java/com/testmaker/api/dto/auth/NewPasswordRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/NewPasswordRequest.java
@@ -1,4 +1,11 @@
 package com.testmaker.api.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class NewPasswordRequest {
+    private String password;
+    private String confirmPassword;
 }
