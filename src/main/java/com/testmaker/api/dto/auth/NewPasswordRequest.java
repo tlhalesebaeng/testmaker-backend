13c21161f641e955e299fd48aaf107e91f87dd85
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 09:52:36 +0200

    Add the password reset code on the new password request and validate it

--- src/main/java/com/testmaker/api/dto/auth/NewPasswordRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/NewPasswordRequest.java
@@ -1,11 +1,18 @@
 package com.testmaker.api.dto.auth;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class NewPasswordRequest {
+    @NotNull(message = "Invalid code provided! Please provide a valid code")
+    @Min(value = 100000, message = "Invalid code provided! Please provide a 6 digit code")
+    @Max(value = 999999, message = "Invalid code provided! Please provide a 6 digit code")
+    private String code;
     private String password;
     private String confirmPassword;
 }
