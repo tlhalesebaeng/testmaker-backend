517b53388496b561f2f4290b83f7e47765fa8b29
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 09:16:54 +0200

    Add data methods, the all args constructor and define the code field

--- src/main/java/com/testmaker/api/dto/auth/ConfirmCodeRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/ConfirmCodeRequest.java
@@ -1,4 +1,10 @@
 package com.testmaker.api.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ConfirmCodeRequest {
+    private Integer code;
 }
