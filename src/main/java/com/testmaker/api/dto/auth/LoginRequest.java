1fa6899c24c203209efe77e183fe242bac06ccff
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-14 09:00:01 +0200

    Add the not blank validator on fields

--- src/main/java/com/testmaker/api/dto/auth/LoginRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/LoginRequest.java
@@ -1,11 +1,15 @@
 package com.testmaker.api.dto.auth;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class LoginRequest {
+    @NotBlank(message = "Invalid username provided! Please provide a valid username")
     private String username;
+
+    @NotBlank(message = "Invalid password provided! Please provide a valid password")
     private String password;
 }
