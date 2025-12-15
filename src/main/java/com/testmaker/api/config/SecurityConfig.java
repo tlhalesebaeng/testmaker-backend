c0bb7d44cf8d8cddb9ab1c13956d046250baae17
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-15 12:50:09 +0200

    Set the authentication provider of spring security to dao authentication provider that uses the pricipal user details service

--- src/main/java/com/testmaker/api/config/SecurityConfig.java
+++ src/main/java/com/testmaker/api/config/SecurityConfig.java
@@ -1,22 +1,29 @@
 package com.testmaker.api.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(AbstractHttpConfigurer::disable);
         http.httpBasic(AbstractHttpConfigurer::disable);
         http.sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authenticationProvider(new DaoAuthenticationProvider(userDetailsService));
         return http.build();
     }
 }
