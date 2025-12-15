24c1dce21c7faafcded88eb339365e7e93035a1e
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-15 12:58:51 +0200

    Prepare to implement spring security's user details interface

--- src/main/java/com/testmaker/api/service/user/PrincipalUserDetails.java
+++ src/main/java/com/testmaker/api/service/user/PrincipalUserDetails.java
@@ -1,4 +1,24 @@
 package com.testmaker.api.service.user;
 
-public class PrincipalUserDetails {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PrincipalUserDetails implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
