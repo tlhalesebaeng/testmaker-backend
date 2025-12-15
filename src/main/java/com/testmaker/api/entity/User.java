c17404eacfa6b38e0b5d4665a0a46ad0cc11ae19
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-15 13:01:13 +0200

    Create getters and setters, no args constructor, make this class an entity, define user fields

--- src/main/java/com/testmaker/api/entity/User.java
+++ src/main/java/com/testmaker/api/entity/User.java
@@ -1,4 +1,23 @@
 package com.testmaker.api.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
 }
