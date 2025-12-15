e4af1643e0992d0f649e9469af7111a93f75e0f3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-15 13:02:44 +0200

    Add the all args constructor, define the user field and return the user password and username

--- src/main/java/com/testmaker/api/service/user/PrincipalUserDetails.java
+++ src/main/java/com/testmaker/api/service/user/PrincipalUserDetails.java
@@ -1,12 +1,17 @@
 package com.testmaker.api.service.user;
 
+import com.testmaker.api.entity.User;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@AllArgsConstructor
 public class PrincipalUserDetails implements UserDetails {
+    private User user;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
@@ -14,11 +19,11 @@ public class PrincipalUserDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return "";
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return user.getUsername();
     }
 }
