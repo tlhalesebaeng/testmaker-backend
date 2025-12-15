83dd7af698fb88cf3dfee5de4a4879f209caf595
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-15 12:36:17 +0200

    Make this file a bean source file, enable web security and create a security filter chain bean

--- src/main/java/com/testmaker/api/config/SecurityConfig.java
+++ src/main/java/com/testmaker/api/config/SecurityConfig.java
@@ -1,4 +1,16 @@
 package com.testmaker.api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return null;
+    }
 }
