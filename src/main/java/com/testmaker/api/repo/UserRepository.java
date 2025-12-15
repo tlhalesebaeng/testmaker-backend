04244f8650ef16a9df6bf3f011e9c1785dab2a59
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-15 12:54:27 +0200

    Change class to an interface and extend the jpa repository interface

--- src/main/java/com/testmaker/api/repo/UserRepository.java
+++ src/main/java/com/testmaker/api/repo/UserRepository.java
@@ -1,4 +1,7 @@
 package com.testmaker.api.repo;
 
-public class UserRepository {
+import com.testmaker.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Long, User> {
 }
