54156167a71d95ff878fee61ac3ac235086bcc31
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 10:10:03 +0200

    Implement the create user method

--- src/main/java/com/testmaker/api/service/user/UserService.java
+++ src/main/java/com/testmaker/api/service/user/UserService.java
@@ -2,14 +2,21 @@ package com.testmaker.api.service.user;
 
 import com.testmaker.api.dto.auth.SignupRequest;
 import com.testmaker.api.entity.User;
+import com.testmaker.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserServiceInterface{
+    private final UserRepository userRepo;
+
     @Override
     public User createUser(SignupRequest requestDto) {
-        return null;
+        User user = new User();
+        user.setEmail(requestDto.getEmail());
+        user.setUsername(requestDto.getUsername());
+        user.setPassword(requestDto.getPassword());
+        return userRepo.save(user);
     }
 }
