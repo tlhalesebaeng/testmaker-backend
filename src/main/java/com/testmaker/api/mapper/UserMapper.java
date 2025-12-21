eea887e6c0fdc510f8d265471c5602ab7d5b8e69
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 10:09:26 +0200

    Create a method to convert a user object to a user response dto

--- src/main/java/com/testmaker/api/mapper/UserMapper.java
+++ src/main/java/com/testmaker/api/mapper/UserMapper.java
@@ -1,4 +1,13 @@
 package com.testmaker.api.mapper;
 
+import com.testmaker.api.dto.user.UserResponse;
+import com.testmaker.api.entity.User;
+
 public class UserMapper {
+    public static UserResponse toResponse(User user) {
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setUsername(user.getUsername());
+        return response;
+    }
 }
