ea17fad1fcfd22a9862aad4171842891dde049ea
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 10:19:28 +0200

    Create a bcrypt password encoder bean

--- src/main/java/com/testmaker/api/config/AppConfig.java
+++ src/main/java/com/testmaker/api/config/AppConfig.java
@@ -1,7 +1,17 @@
 package com.testmaker.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
+    @Value("${api.password-encoder.strength}")
+    private Integer passwordEncoderStrength;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(passwordEncoderStrength);
+    }
 }
