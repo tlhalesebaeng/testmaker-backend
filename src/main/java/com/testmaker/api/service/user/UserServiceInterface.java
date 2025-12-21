92f4b4d955bf710717f96d66c9bfe1e57dfe86dd
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 09:50:19 +0200

    Add a method to create a new user

--- src/main/java/com/testmaker/api/service/user/UserServiceInterface.java
+++ src/main/java/com/testmaker/api/service/user/UserServiceInterface.java
@@ -1,4 +1,8 @@
 package com.testmaker.api.service.user;
 
+import com.testmaker.api.dto.auth.SignupRequest;
+import com.testmaker.api.entity.User;
+
 public interface UserServiceInterface {
+    User createUser(SignupRequest requestDto);
 }
