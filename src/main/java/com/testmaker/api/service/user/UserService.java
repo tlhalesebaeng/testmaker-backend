3c08ae6b1f13f8e0f6267e4c248c48a499969d58
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 10:19:57 +0200

    Use the password encoder to encode the incoming user password

--- src/main/java/com/testmaker/api/service/user/UserService.java
+++ src/main/java/com/testmaker/api/service/user/UserService.java
@@ -4,19 +4,21 @@ import com.testmaker.api.dto.auth.SignupRequest;
 import com.testmaker.api.entity.User;
 import com.testmaker.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserServiceInterface{
     private final UserRepository userRepo;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public User createUser(SignupRequest requestDto) {
         User user = new User();
         user.setEmail(requestDto.getEmail());
         user.setUsername(requestDto.getUsername());
-        user.setPassword(requestDto.getPassword());
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
         return userRepo.save(user);
     }
 }
