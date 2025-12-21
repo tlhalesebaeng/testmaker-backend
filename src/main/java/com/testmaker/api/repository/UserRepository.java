171dba43bec2c99b9428bba24b8507010318e9eb
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 13:18:11 +0200

    Create a JPA finder method to find user by email verification code and status

--- src/main/java/com/testmaker/api/repository/UserRepository.java
+++ src/main/java/com/testmaker/api/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.testmaker.api.repository;
 
 import com.testmaker.api.entity.User;
+import com.testmaker.api.utils.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmailVerificationCodeAndStatus(Integer code, Status status);
 }
