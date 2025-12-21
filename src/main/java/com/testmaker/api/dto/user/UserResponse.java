1ee7c4ba400fc1862e0e804d82fa827690a60113
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 12:15:56 +0200

    Define a status field

--- src/main/java/com/testmaker/api/dto/user/UserResponse.java
+++ src/main/java/com/testmaker/api/dto/user/UserResponse.java
@@ -1,5 +1,6 @@
 package com.testmaker.api.dto.user;
 
+import com.testmaker.api.utils.Status;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,4 +9,5 @@ import lombok.NoArgsConstructor;
 public class UserResponse {
     private Long id;
     private String username;
+    private Status status;
 }
