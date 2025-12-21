3ef85762637d260a2e0a65ee5142c0bc3dc5ef18
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 13:17:26 +0200

    Implement the verify email address method

--- src/main/java/com/testmaker/api/service/user/UserService.java
+++ src/main/java/com/testmaker/api/service/user/UserService.java
@@ -1,13 +1,17 @@
 package com.testmaker.api.service.user;
 
+import com.testmaker.api.dto.auth.ConfirmCodeRequest;
 import com.testmaker.api.dto.auth.SignupRequest;
 import com.testmaker.api.entity.User;
+import com.testmaker.api.exception.InvalidVerificationCodeException;
 import com.testmaker.api.repository.UserRepository;
 import com.testmaker.api.utils.Code;
 import com.testmaker.api.utils.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +28,14 @@ public class UserService implements UserServiceInterface{
         user.setStatus(Status.PENDING_EMAIL_VERIFICATION);
         // TODO: Send the user an email with the 6 digit verification code
         user.setEmailVerificationCode(Code.generate());
+        return userRepo.save(user);
+    }
+
+    @Override
+    public User verifyEmailAddress(ConfirmCodeRequest requestDto) {
+        Optional<User> optionalUser = userRepo.findByEmailVerificationCodeAndStatus(requestDto.getCode(), Status.PENDING_EMAIL_VERIFICATION);
+        User user = optionalUser.orElseThrow(() -> new InvalidVerificationCodeException("Invalid code provided! Please check your code and try again"));
+        user.setStatus(Status.ACTIVE);
         return userRepo.save(user);
     }
 }
