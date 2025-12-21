f48874d0084643c1f45ae98bb870de2b81292d11
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 08:41:46 +0200

    Create a method to find a user by username

--- src/main/java/com/testmaker/api/repository/UserRepository.java
+++ src/main/java/com/testmaker/api/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.testmaker.api.repository;
 import com.testmaker.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Long, User> {
+    Optional<User> findByUsername(String username);
 }
