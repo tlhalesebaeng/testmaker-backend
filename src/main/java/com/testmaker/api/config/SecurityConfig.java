a0839e3b2560525d4f0d35ee2c3942d13e194607
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 09:46:17 +0200

    Use the custom cors configuration source to configure cors

--- src/main/java/com/testmaker/api/config/SecurityConfig.java
+++ src/main/java/com/testmaker/api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.testmaker.api.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,18 +11,21 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+    private final @Qualifier("corsConfig") CorsConfigurationSource corsConfigSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(AbstractHttpConfigurer::disable);
         http.httpBasic(AbstractHttpConfigurer::disable);
+        http.cors(customizer -> customizer.configurationSource(corsConfigSource));
         http.sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authenticationProvider(new DaoAuthenticationProvider(userDetailsService));
         return http.build();
