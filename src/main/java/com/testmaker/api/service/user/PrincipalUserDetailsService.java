815831e179708e7551dade6c185573ec20df2762
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 08:42:49 +0200

    Implement the load user by username method

--- src/main/java/com/testmaker/api/service/user/PrincipalUserDetailsService.java
+++ src/main/java/com/testmaker/api/service/user/PrincipalUserDetailsService.java
@@ -1,14 +1,24 @@
 package com.testmaker.api.service.user;
 
+import com.testmaker.api.entity.User;
+import com.testmaker.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class PrincipalUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepo;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<User> optionalUser = userRepo.findByUsername(username);
+        User user = optionalUser.orElseThrow(() -> new UsernameNotFoundException("No user was found with this username. Please check your username and try again"));
+        return new PrincipalUserDetails(user);
     }
 }
