abde0842a6809953504ea60ac5114064d8d674c7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 09:45:42 +0200

    Implement the get configuration source method

--- src/main/java/com/testmaker/api/config/CorsConfig.java
+++ src/main/java/com/testmaker/api/config/CorsConfig.java
@@ -1,14 +1,27 @@
 package com.testmaker.api.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.List;
+
 @Component
 public class CorsConfig implements CorsConfigurationSource {
+    @Value("${frontend-base-url}")
+    private String frontendBaseUrl;
+
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-        return null;
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedOrigins(List.of(frontendBaseUrl));
+        corsConfig.setAllowedMethods(List.of("*"));
+        corsConfig.setAllowedHeaders(List.of("*"));
+
+        return corsConfig;
     }
 }
