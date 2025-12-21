bba2d39101f98b668cddff71063e0509ca7accc3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 09:52:49 +0200

    Add the required args constructor

--- src/main/java/com/testmaker/api/service/user/UserService.java
+++ src/main/java/com/testmaker/api/service/user/UserService.java
@@ -2,9 +2,11 @@ package com.testmaker.api.service.user;
 
 import com.testmaker.api.dto.auth.SignupRequest;
 import com.testmaker.api.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService implements UserServiceInterface{
     @Override
     public User createUser(SignupRequest requestDto) {
