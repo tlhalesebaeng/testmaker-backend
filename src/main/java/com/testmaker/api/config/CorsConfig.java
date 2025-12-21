24088bbd3e3fab6818f84a32d700fb1e08ef9f8f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 09:28:37 +0200

    Make the class a bean

--- src/main/java/com/testmaker/api/config/CorsConfig.java
+++ src/main/java/com/testmaker/api/config/CorsConfig.java
@@ -1,9 +1,11 @@
 package com.testmaker.api.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+@Component
 public class CorsConfig implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
