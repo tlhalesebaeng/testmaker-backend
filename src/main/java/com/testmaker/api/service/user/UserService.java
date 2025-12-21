2cff201eb8523bdf7958cc8c2c2f2ad34969e158
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-21 12:35:00 +0200

    Generate the random 6 digit code and set it before saving user details

--- src/main/java/com/testmaker/api/service/user/UserService.java
+++ src/main/java/com/testmaker/api/service/user/UserService.java
@@ -3,6 +3,7 @@ package com.testmaker.api.service.user;
 import com.testmaker.api.dto.auth.SignupRequest;
 import com.testmaker.api.entity.User;
 import com.testmaker.api.repository.UserRepository;
+import com.testmaker.api.utils.Code;
 import com.testmaker.api.utils.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +22,8 @@ public class UserService implements UserServiceInterface{
         user.setUsername(requestDto.getUsername());
         user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
         user.setStatus(Status.PENDING_EMAIL_VERIFICATION);
+        // TODO: Send the user an email with the 6 digit verification code
+        user.setEmailVerificationCode(Code.generate());
         return userRepo.save(user);
     }
 }
