8d15fd29859c3ecc9cf3df0a92a4d1011f835c16
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-22 15:32:58 +0200

    Set the email verification code expiration before saving user details

--- src/main/java/com/testmaker/api/service/user/UserService.java
+++ src/main/java/com/testmaker/api/service/user/UserService.java
@@ -8,9 +8,11 @@ import com.testmaker.api.repository.UserRepository;
 import com.testmaker.api.utils.Code;
 import com.testmaker.api.utils.Status;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -18,6 +20,9 @@ import java.util.Optional;
 public class UserService implements UserServiceInterface{
     private final UserRepository userRepo;
     private final BCryptPasswordEncoder passwordEncoder;
+
+    @Value("${api.email-verification-code.expiration}")
+    private Integer emailVerificationCodeExpiration;
 
     @Override
     public User createUser(SignupRequest requestDto) {
@@ -28,6 +33,7 @@ public class UserService implements UserServiceInterface{
         user.setStatus(Status.PENDING_EMAIL_VERIFICATION);
         // TODO: Send the user an email with the 6 digit verification code
         user.setEmailVerificationCode(Code.generate());
+        user.setEmailVerificationCodeExpiration(LocalDateTime.now().plusMinutes(emailVerificationCodeExpiration));
         return userRepo.save(user);
     }
 
