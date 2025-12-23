1e489157624ca0b0ccdf3a0708daf3b8e9660f93
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 07:40:34 +0200

    Implement the generate token method

--- src/main/java/com/testmaker/api/service/jwt/JwtService.java
+++ src/main/java/com/testmaker/api/service/jwt/JwtService.java
@@ -1,4 +1,35 @@
 package com.testmaker.api.service.jwt;
 
+import com.testmaker.api.entity.User;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.util.Date;
+
 public class JwtService implements JwtServiceInterface{
+    @Value("${api.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${api.jwt.expiration}")
+    private Long jwtExpiration; // In milliseconds
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @Override
+    public String generateToken(User user) {
+        JwtBuilder jwtBuilder = Jwts.builder();
+        jwtBuilder.setSubject(user.getUsername());
+        jwtBuilder.setIssuedAt(new Date(System.currentTimeMillis()));
+        jwtBuilder.setExpiration(new Date(System.currentTimeMillis() + jwtExpiration));
+        jwtBuilder.signWith(this.getKey(), SignatureAlgorithm.HS256);
+        return jwtBuilder.compact();
+    }
 }
