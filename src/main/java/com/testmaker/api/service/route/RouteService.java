ab387608d26ba4e4729bfe794b55e7ae13cf75f0
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 08:13:31 +0200

    Implement the get protected routes method

--- src/main/java/com/testmaker/api/service/route/RouteService.java
+++ src/main/java/com/testmaker/api/service/route/RouteService.java
@@ -1,4 +1,12 @@
 package com.testmaker.api.service.route;
 
+import com.testmaker.api.utils.Route;
+
+import java.util.List;
+
 public class RouteService implements RouteServiceInterface{
+    @Override
+    public List<Route> getProtected() {
+        return List.of();
+    }
 }
