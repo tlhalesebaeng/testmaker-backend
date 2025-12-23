29882d33eda2fa4bce408a4e27e433a6fd7799cc
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 07:54:31 +0200

    Implement the method to create a cookie

--- src/main/java/com/testmaker/api/service/cookie/CookieService.java
+++ src/main/java/com/testmaker/api/service/cookie/CookieService.java
@@ -1,4 +1,15 @@
 package com.testmaker.api.service.cookie;
 
+import jakarta.servlet.http.Cookie;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CookieService implements CookieServiceInterface{
+    @Override
+    public Cookie create(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.isHttpOnly();
+        cookie.setPath("/");
+        return cookie;
+    }
 }
