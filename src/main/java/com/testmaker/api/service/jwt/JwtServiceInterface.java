ed0f940b9a9eb69d130f2776e9a5ed106888d8a0
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 08:35:58 +0200

    Define a method to get all claims from a token

--- src/main/java/com/testmaker/api/service/jwt/JwtServiceInterface.java
+++ src/main/java/com/testmaker/api/service/jwt/JwtServiceInterface.java
@@ -1,7 +1,9 @@
 package com.testmaker.api.service.jwt;
 
 import com.testmaker.api.entity.User;
+import io.jsonwebtoken.Claims;
 
 public interface JwtServiceInterface {
     String generateToken(User user);
+    Claims getAllClaims(String token);
 }
