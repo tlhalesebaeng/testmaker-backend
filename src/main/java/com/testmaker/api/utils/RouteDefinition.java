5a35da2121c06af0e055d6d686705c9070bd4159
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 08:06:01 +0200

    Define fields

--- src/main/java/com/testmaker/api/utils/RouteDefinition.java
+++ src/main/java/com/testmaker/api/utils/RouteDefinition.java
@@ -1,4 +1,9 @@
 package com.testmaker.api.utils;
 
+import org.springframework.util.AntPathMatcher;
+
 public class RouteDefinition {
+    private String URI;
+    private String method;
+    private AntPathMatcher matcher;
 }
