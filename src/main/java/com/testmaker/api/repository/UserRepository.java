3db3f1dde629f1b6a6303181beddb5e01829972f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 07:23:01 +0200

    Change the method name and add a custom jpql to find the user

--- src/main/java/com/testmaker/api/repository/UserRepository.java
+++ src/main/java/com/testmaker/api/repository/UserRepository.java
@@ -3,10 +3,14 @@ package com.testmaker.api.repository;
 import com.testmaker.api.entity.User;
 import com.testmaker.api.utils.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByEmailVerificationCodeAndStatus(Integer code, Status status);
+
+    @Query("SELECT u FROM User u WHERE u.emailVerificationCode = :code AND u.emailVerificationCodeExpiration > :date AND u.status = :status")
+    Optional<User> findByValidEmailVerificationCode(Integer code, LocalDateTime date, Status status);
 }
