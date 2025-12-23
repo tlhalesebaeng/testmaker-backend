3a8c08865ab518711af0af3a1e65a9abe425f80c
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 08:16:41 +0200

    Implement the method that checks if a route is protected or not

--- src/main/java/com/testmaker/api/service/route/RouteService.java
+++ src/main/java/com/testmaker/api/service/route/RouteService.java
@@ -9,4 +9,13 @@ public class RouteService implements RouteServiceInterface{
     public List<Route> getProtected() {
         return List.of();
     }
+
+    @Override
+    public boolean isProtected(String URI, String method) {
+        for(Route route : this.getProtected()) {
+            if(route.matches(method, URI)) return true;
+        }
+
+        return false;
+    }
 }
