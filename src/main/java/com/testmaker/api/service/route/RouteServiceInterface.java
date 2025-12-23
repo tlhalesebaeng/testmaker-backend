ee6732a55e00ea87a6f18572144839067d5da143
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 08:10:53 +0200

    Create a method to get all protected routes

--- src/main/java/com/testmaker/api/service/route/RouteServiceInterface.java
+++ src/main/java/com/testmaker/api/service/route/RouteServiceInterface.java
@@ -1,4 +1,10 @@
 package com.testmaker.api.service.route;
 
+
+import com.testmaker.api.utils.Route;
+
+import java.util.List;
+
 public interface RouteServiceInterface {
+    List<Route> getProtected();
 }
