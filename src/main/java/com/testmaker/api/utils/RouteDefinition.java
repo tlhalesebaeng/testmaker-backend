adfb1325c713af4ebf4a7650db901350edd0e4b5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-23 08:06:58 +0200

    Create a constructor that takes a uri and the method as strings

--- src/main/java/com/testmaker/api/utils/RouteDefinition.java
+++ src/main/java/com/testmaker/api/utils/RouteDefinition.java
@@ -6,4 +6,10 @@ public class RouteDefinition {
     private String URI;
     private String method;
     private AntPathMatcher matcher;
+
+    public RouteDefinition(String URI, String method) {
+        this.URI = URI;
+        this.method = method;
+        this.matcher = new AntPathMatcher();
+    }
 }
