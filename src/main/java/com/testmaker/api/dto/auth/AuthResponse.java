492228a5210a5de56c7a756635cb1528bdb400a9
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-24 16:48:01 +0200

    Add data methods and the all args constructor

--- src/main/java/com/testmaker/api/dto/auth/AuthResponse.java
+++ src/main/java/com/testmaker/api/dto/auth/AuthResponse.java
@@ -1,7 +1,11 @@
 package com.testmaker.api.dto.auth;
 
 import com.testmaker.api.dto.user.UserResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class AuthResponse {
     private Boolean isAuth;
     private UserResponse user;
