2f2446d5d4969013039876bf6248122c49d6c42d
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-24 20:48:54 +0200

    Define a method that will be used to login the user

--- src/main/java/com/testmaker/api/service/user/UserServiceInterface.java
+++ src/main/java/com/testmaker/api/service/user/UserServiceInterface.java
@@ -1,6 +1,7 @@
 package com.testmaker.api.service.user;
 
 import com.testmaker.api.dto.auth.ConfirmCodeRequest;
+import com.testmaker.api.dto.auth.LoginRequest;
 import com.testmaker.api.dto.auth.SignupRequest;
 import com.testmaker.api.entity.User;
 
@@ -8,4 +9,5 @@ public interface UserServiceInterface {
     User createUser(SignupRequest requestDto);
     User verifyEmailAddress(ConfirmCodeRequest requestDto);
     User checkAuth(String jwt);
+    User login(LoginRequest requestDto);
 }
