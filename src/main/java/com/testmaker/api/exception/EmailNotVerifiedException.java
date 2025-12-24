4595deb9e9de9d24b6941f0a0a0517a6ec7a0d5b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-24 20:47:59 +0200

    Create a constructor that accepts a message and calls the constructor of the parent class with that message

--- src/main/java/com/testmaker/api/exception/EmailNotVerifiedException.java
+++ src/main/java/com/testmaker/api/exception/EmailNotVerifiedException.java
@@ -1,4 +1,7 @@
 package com.testmaker.api.exception;
 
 public class EmailNotVerifiedException extends RuntimeException {
+    public EmailNotVerifiedException(String message) {
+        super(message);
+    }
 }
