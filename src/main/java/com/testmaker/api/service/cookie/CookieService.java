7be26ab87336eb83fe7c8f16db6c846916835252
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-25 19:34:05 +0200

    Set the cookie's maxAge when its provided

--- src/main/java/com/testmaker/api/service/cookie/CookieService.java
+++ src/main/java/com/testmaker/api/service/cookie/CookieService.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class CookieService implements CookieServiceInterface{
     @Override
-    public Cookie create(String name, String value) {
+    public Cookie create(String name, String value, Integer maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.isHttpOnly();
         cookie.setPath("/");
+        if(maxAge != null) cookie.setMaxAge(maxAge);
         return cookie;
     }
 }
