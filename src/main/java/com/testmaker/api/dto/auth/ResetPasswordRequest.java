5844a9f39d8514e419a4392b74f3331edf7da756
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-27 20:04:26 +0200

    Add not blank validators on fields

--- src/main/java/com/testmaker/api/dto/auth/ResetPasswordRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/ResetPasswordRequest.java
@@ -1,11 +1,15 @@
 package com.testmaker.api.dto.auth;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class ResetPasswordRequest {
+    @NotBlank(message = "Password required! Please provide a valid password")
     private String password;
+    
+    @NotBlank(message = "Confirm password required! Please confirm your password")
     private String confirmPassword;
 }
