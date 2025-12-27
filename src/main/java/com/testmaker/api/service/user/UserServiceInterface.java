a2e209fdb53353519a555f341a995e15a9ef9b3f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-27 19:31:17 +0200

    Add a method that will reset the user password

--- src/main/java/com/testmaker/api/service/user/UserServiceInterface.java
+++ src/main/java/com/testmaker/api/service/user/UserServiceInterface.java
@@ -2,12 +2,14 @@ package com.testmaker.api.service.user;
 
 import com.testmaker.api.dto.auth.ConfirmCodeRequest;
 import com.testmaker.api.dto.auth.LoginRequest;
+import com.testmaker.api.dto.auth.ResetPasswordRequest;
 import com.testmaker.api.dto.auth.SignupRequest;
 import com.testmaker.api.entity.User;
 
 public interface UserServiceInterface {
     User createUser(SignupRequest requestDto);
     User verifyEmailAddress(ConfirmCodeRequest requestDto);
+    User resetPassword(ResetPasswordRequest requestDto);
     User checkAuth(String jwt);
     User login(LoginRequest requestDto);
     void logout();
