0beb2adfcea1e8d95b35506a2f7156326ebf3b12
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-27 20:03:24 +0200

    Add data methods and the all args constructor

--- src/main/java/com/testmaker/api/dto/auth/ResetPasswordRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/ResetPasswordRequest.java
@@ -1,5 +1,10 @@
 package com.testmaker.api.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ResetPasswordRequest {
     private String password;
     private String confirmPassword;
