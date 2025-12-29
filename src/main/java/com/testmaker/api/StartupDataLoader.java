beb9b0fea0f001902554a24a22eb2db205242cec
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-29 18:02:03 +0200

    Add the required args constructor

--- src/main/java/com/testmaker/api/StartupDataLoader.java
+++ src/main/java/com/testmaker/api/StartupDataLoader.java
@@ -1,7 +1,9 @@
 package com.testmaker.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class StartupDataLoader {
 }
