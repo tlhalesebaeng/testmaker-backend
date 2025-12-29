2e2c7ffb42e08613e55ee3af092c5a9ca93d4b44
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-29 15:42:19 +0200

    Auto generate the status id

--- src/main/java/com/testmaker/api/entity/Status.java
+++ src/main/java/com/testmaker/api/entity/Status.java
@@ -1,6 +1,9 @@
 package com.testmaker.api.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +13,8 @@ import lombok.Setter;
 @Entity
 @NoArgsConstructor
 public class Status {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
