1c45f759572f659f7a8e2b9e059416df1f449bd7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-29 18:26:23 +0200

    Create a method that inserts all statuses that do not exists into the database on app startup

--- src/main/java/com/testmaker/api/StartupDataLoader.java
+++ src/main/java/com/testmaker/api/StartupDataLoader.java
@@ -1,9 +1,29 @@
 package com.testmaker.api;
 
+import com.testmaker.api.entity.Status;
+import com.testmaker.api.repository.StatusRepository;
+import com.testmaker.api.utils.AccountStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class StartupDataLoader {
+    private final StatusRepository statusRepo;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void insertStatusData() {
+        // Ensure that the statuses we are inserting do not exist in the database
+        List<Status> statuses = Arrays.stream(AccountStatus.values())
+                .map(s -> new Status(s.name()))
+                .filter(s -> !statusRepo.existsByName(s.getName()))
+                .toList();
+
+        statusRepo.saveAll(statuses);
+    }
 }
