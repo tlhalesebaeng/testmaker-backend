18ba9f1f82443706085f58bf702310cdfaf42282
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-29 15:41:01 +0200

    Add annotation for getters, setters, entity and no args constructor

--- src/main/java/com/testmaker/api/entity/Status.java
+++ src/main/java/com/testmaker/api/entity/Status.java
@@ -1,5 +1,14 @@
 package com.testmaker.api.entity;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class Status {
     private Long id;
     private String name;
