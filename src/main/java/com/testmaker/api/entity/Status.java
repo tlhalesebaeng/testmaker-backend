438e56ee57520a8df32314719135987d92f73d4b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-29 15:57:28 +0200

    Change the name of the status to be an enum type

--- src/main/java/com/testmaker/api/entity/Status.java
+++ src/main/java/com/testmaker/api/entity/Status.java
@@ -1,5 +1,6 @@
 package com.testmaker.api.entity;
 
+import com.testmaker.api.utils.AccountStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,9 @@ public class Status {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private AccountStatus name;
 
     @OneToMany(mappedBy = "status")
     private Collection<User> user;
