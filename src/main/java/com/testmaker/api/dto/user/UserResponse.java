de807d76df8964b1ad19326393f73b1bf247d11d
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-29 18:25:08 +0200

    Change the returned status to a string

--- src/main/java/com/testmaker/api/dto/user/UserResponse.java
+++ src/main/java/com/testmaker/api/dto/user/UserResponse.java
@@ -1,6 +1,5 @@
 package com.testmaker.api.dto.user;
 
-import com.testmaker.api.utils.AccountStatus;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,5 +8,5 @@ import lombok.NoArgsConstructor;
 public class UserResponse {
     private Long id;
     private String username;
-    private AccountStatus status;
+    private String status;
 }
--- src/main/java/com/testmaker/api/mapper/UserMapper.java
+++ src/main/java/com/testmaker/api/mapper/UserMapper.java
@@ -8,7 +8,7 @@ public class UserMapper {
         UserResponse response = new UserResponse();
         response.setId(user.getId());
         response.setUsername(user.getUsername());
-        response.setStatus(user.getStatus());
+        response.setStatus(user.getStatus().getName());
         return response;
     }
 }
