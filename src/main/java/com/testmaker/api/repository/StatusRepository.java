7071e98fd27d5b5a8683a0145134bded51ea1149
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-29 17:55:14 +0200

    Add a find by name finder method

--- src/main/java/com/testmaker/api/repository/StatusRepository.java
+++ src/main/java/com/testmaker/api/repository/StatusRepository.java
@@ -1,7 +1,11 @@
 package com.testmaker.api.repository;
 
 import com.testmaker.api.entity.Status;
+import com.testmaker.api.utils.AccountStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StatusRepository extends JpaRepository<Status, Long> {
+    Optional<Status> findByName(AccountStatus accountStatus);
 }
