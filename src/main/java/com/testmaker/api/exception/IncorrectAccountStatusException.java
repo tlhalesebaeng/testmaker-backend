f6f20a5b8c184a42c47b18645312033406b19462
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-31 12:56:41 +0200

    Initial commit

---
+++ src/main/java/com/testmaker/api/exception/IncorrectAccountStatusException.java
@@ -0,0 +1,7 @@
+package com.testmaker.api.exception;
+
+public class IncorrectAccountStatusException extends RuntimeException {
+  public IncorrectAccountStatusException(String message) {
+    super(message);
+  }
+}
