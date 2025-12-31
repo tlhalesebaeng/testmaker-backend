e08c948cfbdc57b66ee059eb6b705c6aa32739b7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-31 12:35:46 +0200

    Change the status type from account status

--- src/main/java/com/testmaker/api/repository/UserRepository.java
+++ src/main/java/com/testmaker/api/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.testmaker.api.repository;
 
+import com.testmaker.api.entity.Status;
 import com.testmaker.api.entity.User;
-import com.testmaker.api.utils.AccountStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,7 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     @Query("SELECT u FROM User u WHERE u.emailVerificationCode = :code AND u.emailVerificationCodeExpiration > :date AND u.status = :status")
-    Optional<User> findByValidEmailVerificationCode(Integer code, LocalDateTime date, AccountStatus status);
+    Optional<User> findByValidEmailVerificationCode(Integer code, LocalDateTime date, Status status);
 
     @Query("SELECT u From User u WHERE u.passwordResetCode = :code AND u.passwordResetCodeExpiration > :date")
     Optional<User> findByValidPasswordResetCode(Integer code, LocalDateTime date);
