0d43e0ed137d83b4f7a2cb6ed3789c6c9c723cd1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-31 13:56:40 +0200

    Catch all data integrity violation exceptions

--- src/main/java/com/testmaker/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/testmaker/api/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.testmaker.api.exception;
 
 import com.testmaker.api.dto.exception.ExceptionResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -31,6 +33,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({ UsernameNotFoundException.class, BadCredentialsException.class })
     public ResponseEntity<ExceptionResponse> handleUsernameNotFoundException(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler({ DuplicateKeyException.class })
+    public ResponseEntity<ExceptionResponse> handleDataIntegrityViolationExceptions(DataIntegrityViolationException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(e.getMessage()));
     }
 }
