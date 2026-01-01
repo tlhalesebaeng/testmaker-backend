a1b82173c77bca10600693eeac4ad74e91da8886
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-01 18:25:02 +0200

    Extend the jpa respository interface

--- src/main/java/com/testmaker/api/repository/EmailVerificationCodeRepository.java
+++ src/main/java/com/testmaker/api/repository/EmailVerificationCodeRepository.java
@@ -1,4 +1,7 @@
 package com.testmaker.api.repository;
 
-public interface EmailVerificationCodeRepository {
+import com.testmaker.api.entity.EmailVerificationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailVerificationCodeRepository extends JpaRepository<EmailVerificationCode, Long> {
 }
