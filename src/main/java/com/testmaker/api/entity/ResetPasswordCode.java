f36d1aaaf803e2a3a5997114eb59dc4e17b93db8
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-01 19:14:05 +0200

    Add the code and expiration fields

--- src/main/java/com/testmaker/api/entity/ResetPasswordCode.java
+++ src/main/java/com/testmaker/api/entity/ResetPasswordCode.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Entity
@@ -16,4 +18,7 @@ public class ResetPasswordCode {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Integer code;
+    private LocalDateTime expiration;
 }
