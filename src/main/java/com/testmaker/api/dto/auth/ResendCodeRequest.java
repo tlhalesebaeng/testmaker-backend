d8bfade587293243df2b10d50e49cff6895baece
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-01 19:43:12 +0200

    Add the data annotation, the all args constructor and define a username field with the not blank validator

--- src/main/java/com/testmaker/api/dto/auth/ResendCodeRequest.java
+++ src/main/java/com/testmaker/api/dto/auth/ResendCodeRequest.java
@@ -1,4 +1,12 @@
 package com.testmaker.api.dto.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ResendCodeRequest {
+    @NotBlank(message = "Username required! Please provide a valid username")
+    private String username;
 }
