d41f7e38353b544c427cc8bcd49c3b902de6a948
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-01 19:33:01 +0200

    Delete the email verification code repository

--- src/main/java/com/testmaker/api/repository/EmailVerificationCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.testmaker.api.repository;
-
-import com.testmaker.api.entity.EmailVerificationCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmailVerificationCodeRepository extends JpaRepository<EmailVerificationCode, Long> {
-}
