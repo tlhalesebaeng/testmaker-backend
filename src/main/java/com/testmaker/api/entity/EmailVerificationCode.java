5e04a58f3a2075b2f00c45033608955f355edb4c
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-01 17:01:02 +0200

    Add the id annotation and auto generate it

--- src/main/java/com/testmaker/api/entity/EmailVerificationCode.java
+++ src/main/java/com/testmaker/api/entity/EmailVerificationCode.java
@@ -1,6 +1,9 @@
 package com.testmaker.api.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +11,10 @@ import lombok.Setter;
 @Setter
 @Entity
 public class EmailVerificationCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private Integer code;
     private Integer expiration;
 }
