3d320d627d48b2ad3e4f4ad80f3e2799f109701c
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-01 16:59:20 +0200

    Add getters, setters and the entity annotation

--- src/main/java/com/testmaker/api/entity/EmailVerificationCode.java
+++ src/main/java/com/testmaker/api/entity/EmailVerificationCode.java
@@ -1,4 +1,11 @@
 package com.testmaker.api.entity;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class EmailVerificationCode {
 }
