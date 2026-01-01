a01cb297e54716570bb0f6dd06d55f20ae2809f2
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-01 19:13:25 +0200

    Make this class an entity and auto generate the id

--- src/main/java/com/testmaker/api/entity/ResetPasswordCode.java
+++ src/main/java/com/testmaker/api/entity/ResetPasswordCode.java
@@ -1,4 +1,19 @@
 package com.testmaker.api.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class ResetPasswordCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
