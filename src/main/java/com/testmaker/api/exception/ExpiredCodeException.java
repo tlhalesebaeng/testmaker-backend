8e25333eb2bd11a4f5a37a5e1a32cbc9fd2f84cf
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-01 16:14:42 +0200

    Create a constructor that takes a message, Extend the runtime exception and call its constructor with the provided message

--- src/main/java/com/testmaker/api/exception/ExpiredCodeException.java
+++ src/main/java/com/testmaker/api/exception/ExpiredCodeException.java
@@ -1,7 +1,7 @@
 package com.testmaker.api.exception;
 
 public class ExpiredCodeException extends RuntimeException {
-  public ExpiredCodeException(String message) {
-    super(message);
-  }
+    public ExpiredCodeException(String message) {
+        super(message);
+    }
 }
