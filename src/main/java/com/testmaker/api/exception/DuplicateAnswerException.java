fbb6d1ecbab7fc8afd242d5b517a7b33f696fedf
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-03 18:36:11 +0200

    Delete duplicate key exception classes

--- src/main/java/com/testmaker/api/exception/DuplicateAnswerException.java
+++
@@ -1,7 +0,0 @@
-package com.testmaker.api.exception;
-
-public class DuplicateAnswerException extends RuntimeException {
-  public DuplicateAnswerException(String message) {
-    super(message);
-  }
-}
--- src/main/java/com/testmaker/api/exception/DuplicateQuestionException.java
+++
@@ -1,7 +0,0 @@
-package com.testmaker.api.exception;
-
-public class DuplicateQuestionException extends RuntimeException {
-  public DuplicateQuestionException(String message) {
-    super(message);
-  }
-}
