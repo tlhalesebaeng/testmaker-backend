145ff9e68fcad09927ecc5c387240cb7e1b8ab36
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-04 08:31:41 +0200

    Create the answer model

--- src/main/java/com/testmaker/api/entity/Answer.java
+++ src/main/java/com/testmaker/api/entity/Answer.java
@@ -1,4 +1,23 @@
 package com.testmaker.api.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    private String answer;
+    private Boolean isCorrect;
 }
