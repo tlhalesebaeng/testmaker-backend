0f8ded7d48a4d5a4e029d488b85e2c2109a664e0
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-04 08:31:21 +0200

    Create the question model

--- src/main/java/com/testmaker/api/entity/Question.java
+++ src/main/java/com/testmaker/api/entity/Question.java
@@ -1,4 +1,31 @@
 package com.testmaker.api.entity;
 
+import com.testmaker.api.utils.QuestionType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id", nullable = false)
+    private Test test;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<Answer> answers;
+
+    @Enumerated(value = EnumType.STRING)
+    private QuestionType type;
+
+    private String question;
 }
