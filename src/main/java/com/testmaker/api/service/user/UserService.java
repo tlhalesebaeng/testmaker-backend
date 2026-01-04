f3637f8a702ce317954fb4fab6183c71576d9dc7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-04 08:30:12 +0200

    Create a method to get a user from the database by username

--- src/main/java/com/testmaker/api/service/user/UserService.java
+++ src/main/java/com/testmaker/api/service/user/UserService.java
@@ -1,4 +1,21 @@
 package com.testmaker.api.service.user;
 
-public class UserService {
+import com.testmaker.api.entity.User;
+import com.testmaker.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserServiceInterface{
+    private final UserRepository userRepo;
+
+    @Override
+    public User getByUsername(String username) {
+        Optional<User> user = userRepo.findByUsername(username);
+        return user.orElseThrow(() -> new UsernameNotFoundException("User not found. Please verify your username and try again"));
+    }
 }
--- src/main/java/com/testmaker/api/service/user/UserServiceInterface.java
+++ src/main/java/com/testmaker/api/service/user/UserServiceInterface.java
@@ -1,4 +1,7 @@
 package com.testmaker.api.service.user;
 
+import com.testmaker.api.entity.User;
+
 public interface UserServiceInterface {
+    User getByUsername(String username);
 }
