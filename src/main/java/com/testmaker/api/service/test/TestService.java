5902d75fbe933106968783ed4a308e647fd6171a
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-04 08:36:17 +0200

    Create a method to create a test

--- src/main/java/com/testmaker/api/service/test/TestService.java
+++ src/main/java/com/testmaker/api/service/test/TestService.java
@@ -1,4 +1,86 @@
 package com.testmaker.api.service.test;
 
-public class TestService {
+import com.testmaker.api.dto.answer.AnswerRequest;
+import com.testmaker.api.dto.question.QuestionRequest;
+import com.testmaker.api.dto.test.CreateTestRequest;
+import com.testmaker.api.entity.Answer;
+import com.testmaker.api.entity.Question;
+import com.testmaker.api.entity.Test;
+import com.testmaker.api.entity.User;
+import com.testmaker.api.repository.TestRepository;
+import com.testmaker.api.service.user.PrincipalUserDetailsService;
+import com.testmaker.api.service.user.UserServiceInterface;
+import com.testmaker.api.utils.QuestionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class TestService implements TestServiceInterface {
+    private final TestRepository testRepo;
+    private final UserServiceInterface userService;
+    private final PrincipalUserDetailsService principalUserService;
+
+    @Override
+    public Test createTest(CreateTestRequest requestDto) {
+        User user = userService.getByUsername(principalUserService.getPrincipal().getUsername());
+
+        Test test = new Test();
+        test.setUser(user);
+        test.setDuration(requestDto.getDuration());
+        test.setTitle(requestDto.getTitle());
+
+        Set<Question> questions = new HashSet<>();
+        Set<String> questionTexts = new HashSet<>(); // Used to detect duplicate questions
+
+        for(QuestionRequest questionRequest : requestDto.getQuestions()) {
+            if(questionTexts.contains(questionRequest.getQuestion())) {
+                throw new DuplicateKeyException("Duplicate question detected! Please make sure that your questions are unique");
+            }
+
+            Question question = new Question();
+            Set<Answer> answers = new HashSet<>();
+            Set<String> answerTexts = new HashSet<>(); // Used to detect duplicate answers
+
+            for(AnswerRequest answerRequest : questionRequest.getAnswers()) {
+                if(answerTexts.contains(answerRequest.getAnswer())) {
+                    throw new DuplicateKeyException("Duplicate answer detected! Please make sure that your answers are unique");
+                }
+
+                Answer answer = new Answer();
+                answer.setQuestion(question);
+                answer.setAnswer(answerRequest.getAnswer());
+                answer.setIsCorrect(answerRequest.getIsCorrect());
+                answers.add(answer);
+
+                answerTexts.add(answer.getAnswer());
+            }
+
+            // Throw an exception when the user provides a type that is not supported
+            switch (questionRequest.getType()) {
+                case "true-or-false":
+                    question.setType(QuestionType.TRUE_OR_FALSE);
+                case "multiple-choice":
+                    question.setType(QuestionType.MULTIPLE_CHOICE);
+                case "fill-the-sentence":
+                    question.setType(QuestionType.FILL_THE_SENTENCE);
+            }
+
+            question.setTest(test);
+            question.setAnswers(answers);
+            question.setQuestion(questionRequest.getQuestion());
+
+            questions.add(question);
+            questionTexts.add(question.getQuestion());
+
+        }
+
+        test.setQuestions(questions);
+
+        return testRepo.save(test);
+    }
 }
--- src/main/java/com/testmaker/api/service/test/TestServiceInterface.java
+++ src/main/java/com/testmaker/api/service/test/TestServiceInterface.java
@@ -1,4 +1,8 @@
 package com.testmaker.api.service.test;
 
+import com.testmaker.api.dto.test.CreateTestRequest;
+import com.testmaker.api.entity.Test;
+
 public interface TestServiceInterface {
+    Test createTest(CreateTestRequest requestDto);
 }
