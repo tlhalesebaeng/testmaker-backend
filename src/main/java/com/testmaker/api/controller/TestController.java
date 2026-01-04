187eea03e810f4c7cfb82899ae0edc1665bd0aad
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-04 08:36:59 +0200

    Expose an endpoint to create a test

--- src/main/java/com/testmaker/api/controller/TestController.java
+++ src/main/java/com/testmaker/api/controller/TestController.java
@@ -1,4 +1,27 @@
 package com.testmaker.api.controller;
 
+import com.testmaker.api.dto.test.CreateTestRequest;
+import com.testmaker.api.dto.test.TestResponse;
+import com.testmaker.api.mapper.TestMapper;
+import com.testmaker.api.service.test.TestServiceInterface;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/test")
 public class TestController {
+    private final TestServiceInterface testService;
+
+    @PostMapping("")
+    public ResponseEntity<TestResponse> createTest(@Valid @RequestBody CreateTestRequest requestDto) {
+        TestResponse response = TestMapper.toResponse(testService.createTest(requestDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
