06ce4c8b1f8e70cb2ea34efb10a7dd4f2c32f383
tlhalesebaeng <tlhalesebaeng@gmail.com>
2026-01-04 08:31:00 +0200

    Create the test model

--- src/main/java/com/testmaker/api/entity/Test.java
+++ src/main/java/com/testmaker/api/entity/Test.java
@@ -1,4 +1,28 @@
 package com.testmaker.api.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "creator")
+    private User user;
+
+    @OneToMany(mappedBy = "test", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<Question> questions;
+
+    private Integer duration;
+    private String title;
 }
